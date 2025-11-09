95f5d2558018d6a7d80b276ecb2b81ea365398eb
sklbz <122096315+sklbz@users.noreply.github.com>
2025-11-09 19:52:38 +0100

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -86,7 +86,8 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     // Guarantee evaluation does not hit the tablebase range
     v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
 
-    return v;
+    // Reverse the evaluation
+    return -v;
 }
 
 // Like evaluate(), but instead of returning a value, it returns
